68c861d4b694c1a6ff1538e8011800462484aa06
Milos <miloskrunic2000@gmail.com>
2025-02-16 19:26:03 +0100

    reseno sa 2 operatora donekle, ima problem kad ide vise u minus

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+
+
 function add(a,b) {
 	return a + b;
 };
@@ -18,8 +22,8 @@ function divide(a,b){
 };
 
 function operate(firstNumber, secondNumber, operator){
-    let num1 = "";
-    let num2
+    isOperatorPressed = false;
+    lastOperator = operator;
     switch(operator){
         case "+": 
             return add(firstNumber,secondNumber);
@@ -39,16 +43,25 @@ function operate(firstNumber, secondNumber, operator){
 const buttons = document.querySelectorAll('input[type="button"]');
 const result = document.getElementById('result');
 
+let operator = "";
+let isOperatorPressed = false;
+let lastOperator = "";
+let currentOperator = "";
+
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         let value = button.value;
         if(value === "="){  
             let splitExpression = result.value.split(/([+\-*/])/);
-            let num1 = parseFloat(splitExpression[0]);
             let operator = splitExpression[1];
-            let num2 = parseFloat(splitExpression[2]);
+            num1 = parseFloat(splitExpression[0]) || num1;  // Ako je NaN, ostavi prethodnu vrednost
+            num2 = parseFloat(splitExpression[2]) || 0;     // Ako nema unosa, postavi na 0
+            
+            console.log(num1, num2, lastOperator, currentOperator, result.value, "ovo se ispisuje posle splita");
             result.value = operate(num1, num2, operator);
+            num1 = parseFloat(result.value);
+
 
         }
         else if(value === "c"){
@@ -56,10 +69,28 @@ buttons.forEach(button => {
             num2 = 0;
             operator = "";
             result.value = "";
-
+            isOperatorPressed = false;
         }
+        else if (value === "+" || value === "-" || value === "*" || value === "/") {
+
+            if (isOperatorPressed){
+                let splitExpression = result.value.split(/([+\-*/])/);
+                num1 = parseFloat(splitExpression[0]);
+                num2 = parseFloat(splitExpression[2]);
+                console.log(num1, num2, lastOperator, currentOperator, result.value, "ovo se ispisuje posle 2 pritiska operatora");
+                result.value = operate(num1, num2, lastOperator);
+                num1 = parseFloat(result.value);
+                num2 = 0; 
+            }
+            result.value += value;
+            currentOperator = value;
+            lastOperator = currentOperator;
+            isOperatorPressed = true;
+
+            }
         else{
             result.value += value;
+            console.log(num1, num2, lastOperator, currentOperator, result.value, "ovo se ispisuje posle pritiskanja broja");
         }
     });
 });
